435dce3ffd7e3626c7708efe2193db251cd58580
Neha <ne.bhatti2@icloud.com>
2024-02-19 23:59:10 +0100

    feat: Fetch and display user-specific books
    - Implemented data fetching from the backend for a specific user using userEmail parameter
    - Utilized useEffect and useState hooks in React to manage asynchronous data retrieval
    - Updated server.js to handle user-specific book requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
 
 import BookListHeader from './components/BookListHeader'
+import { useEffect, useState } from 'react'
 
 
 const App = () => {
+
+  const userEmail = 'neha@gmail.com'
+  const [books, setBooks ] = useState(null)
+
+  //get data
+  const getData = async () => {
+    
+    try {
+      const response = await fetch(`http://localhost:8000/books/${userEmail}`)
+      const json = await response.json()
+      //console.log(json)
+      setBooks(json)
+    }catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => getData, [])
+  
+  console.log(books)
+
+
+
   return (
     <div className="app">
       <BookListHeader listName={'Books List'}/>
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')
+
+app.use(cors())
 
 
 // app.get('/', (req, res) => {
@@ -9,10 +12,14 @@ const pool = require('./db')
 // })
 
 // get all books
-app.get('/books', async (req, res) => {
+app.get('/books/:userEmail', async (req, res) => {
 
-    const userEmail = 'neha@gmail.com'
-    
+    // console.log(req)
+
+    //const userEmail = 'test@gmail.com'
+    const { userEmail } = req.params
+    //console.log(userEmail)
+
     try {
         const books = await pool.query('SELECT * FROM books WHERE user_email = $1', [userEmail])
         res.json(books.rows)
